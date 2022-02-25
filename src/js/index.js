4c9ad46418511b94ba7131b8d8ae17af7380cb68
Siwaaa <siwaa@mail.ru>
2022-02-26 01:13:57 +0300

    add: change info desc card

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,67 @@
+const listInfo = [
+  {
+  title: 'Клиентская база',
+  text_main: 'Все данные по клиенту в одном месте: фотографии, платежи, балансы, договора, штрафы ГИБДД.',
+  text_desc: 'Помогает не терять постоянных клиентов. Вы сможете в любой момент времени проверить, какой водитель в какой момент ездил на автомобиле.'
+  },
+  {
+  title: 'Договора',
+  text_main: 'Сократите время на выдачу автомобиля. Формирование и печать договора в 2 клика',
+  text_desc: 'Больше не нужно вручную заполнять договор. Все документы формируется автоматически из шаблона и моментально отправляются на печать.'
+  },
+  {
+  title: 'Система биллинга',
+  text_main: 'Не нужно считать оплаты вручную и звонить водителю с напоминанием.',
+  text_desc: 'Робот производит автоматическое списание арендной платы согласно тарифу договора. Текущее состояние счета каждого водителя отображается на главном экране.'
+  },
+  {
+  title: 'Штрафы ГИБДД',
+  text_main: 'Вести учет штрафов стало намного проще. Штрафы фиксируются на лицевом счете водителя',
+  text_desc: 'и вы больше не потеряете информацию по ним, а водитель всегда будет в курсе появления новых штрафов. Учет штрафов ГИБДД осуществляется напрямую из государственной базы ГИС ГМП.'
+  },
+  {
+  title: 'Техническое обслуживание',
+  text_main: 'Вся история обслуживания автомобилей, своевременный контроль износа запчастей в связке со складом.',
+  text_desc: 'Предоставьте доступ автосервису или мастеру к карточкам ваших автомобилей, и вы всегда будете в курсе состояния каждой единицы автопарка и ее рентабельности.'
+  },
+      {
+  title: 'Система напоминаний',
+  text_main: 'Внести платеж, сделать ТО, оплатить штраф, заменить полис автострахования – обо всех основных вопросах CRM напомнит автоматически.',
+  text_desc: 'Своевременное напоминание позволяет снизить риск возникновения долга у водителей. Автоматическая блокировка ограничит доступ к автомобилю пока аренда не будет оплачена.'
+  },
+  {
+  title: 'Складской учет',
+  text_main: 'Все запчасти в одном месте, учет установки на автомобили.',
+  text_desc: ''
+  },
+  {
+  title: 'Сотрудники',
+  text_main: 'Несколько уровней доступа: администратор, менеджер, мастер СТО',
+  text_desc: 'Каждый уровень доступа показывает нужную информацию.'
+  },
+  {
+  title: 'Интеграция с Яндекс Такси',
+  text_main: 'Списывайте аренду автоматически прямо с таксометра водителя.',
+  text_desc: 'Система проверит задолжность водителя за аренду и спишет деньги диспетчерской Яндекс Такси.'
+  },
+  {
+  title: 'Отчёты',
+  text_main: 'Вся информация по автомобилям, по водителям, по складу и всему автопарку в 1 месте.',
+  text_desc: 'Отчеты по автомобилям и его рентабельности; по водителям, кто больше принес денег в парк, а кто потратил; по складу: остатки и движение запчастей; сводный отчет и основные показатели.'
+  },
+      {
+  title: 'Фотоосмотры',
+  text_main: 'Системный контроль состояния автомобилей на всех стадиях работы:',
+  text_desc: 'на выдаче, на приеме или в процессе долгосрочной аренды.  Сравнивайте сстояния до и после в интуитивно понятном экране. Все повреждения будут моментально выявляться и фиксироваться за водителем.'
+  },
+  {
+  title: 'Касса',
+  text_main: 'Контролируйте учет движения денежных средств в авто прокате.',
+  text_desc: 'Сколько денег и менеджера, сколько на счету, сколько в кассе. Все видно и удобно планировать. Отчеты по кассе.'
+  }
+]
+
+
 window.onload = function () {
   document.getElementById("link-active").focus();
 }
@@ -74,6 +138,11 @@ function changeHandlerOpportunities(event) {
       t.classList.remove('active')
     })
     document.querySelector('#oppo_img_' + this.value).classList.add('active')
+    const info = document.querySelector('.opportunities__item-info')
+    // формируем плашку с информацией
+    info.children[0].textContent = listInfo[this.value - 1].title
+    info.children[1].textContent = listInfo[this.value - 1].text_main
+    info.children[2].textContent = listInfo[this.value - 1].text_desc
   }
 }
 
@@ -90,10 +159,38 @@ function changeHandlerOpportunitiesMob(event) {
       t.classList.remove('active')
     })
     document.querySelector('#oppo_img_mob_' + event.target.value).classList.add('active')
+    // формируем плашку с информацией
+    const info = document.querySelector('.opportunities__item-info.mobile')
+    info.children[0].textContent = listInfo[event.target.value - 1].title
+    info.children[1].innerHTML = listInfo[event.target.value - 1].text_main + '<br>' + listInfo[event.target.value - 1].text_desc
   }
 }
 
 selectOpportunities.addEventListener('change', changeHandlerOpportunitiesMob);
+
+
+// oppo desc
+
+const oppoDesc = document.querySelector(".oppo_desc");
+const oppoBtn = document.querySelector("#oppo_info_btn");
+
+oppoBtn.addEventListener('click', () => {
+  if(oppoBtn.textContent == "Подробнее") {
+    oppoDesc.style.display = "block"
+    oppoBtn.textContent = "Скрыть"
+  } else {
+    oppoDesc.style.display = "none"
+    oppoBtn.textContent = "Подробнее"
+  }
+})
+
+const oppoDescMob = document.querySelector(".opportunities__item-info.mobile");
+const oppoBtnMob = document.querySelector(".browser-mob__i");
+
+oppoBtnMob.addEventListener('click', () => {
+  oppoDescMob.style.display = oppoDescMob.style.display === 'block' ? 'none' : 'block'
+})
+
 
 // Animation
 
@@ -143,3 +240,18 @@ function changeHandlerInterfaceMob(event) {
 }
 
 selectInterface.addEventListener('change', changeHandlerInterfaceMob);
+
+
+// mess 
+
+const radiosMess = document.querySelectorAll('input[type=radio][name="messengers"]');
+
+function changeHandlerMess(event) {
+  if(this.value) {
+    document.querySelector('#input-mes').placeholder = 'Ваш телефон в ' + this.value
+  }
+}
+
+Array.prototype.forEach.call(radiosMess, function (radio) {
+  radio.addEventListener('change', changeHandlerMess);
+});
