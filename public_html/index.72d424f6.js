5eaa13261f289472f122ec74dc19e86f744406c7
Siwaaa <siwaa@mail.ru>
2022-04-15 16:54:34 +0300

    смена папки билда

--- public_html/index.72d424f6.js
+++ public_html/index.72d424f6.js
@@ -1,2 +1,2 @@
 document.addEventListener("DOMContentLoaded",(()=>{(()=>{const e=document.querySelector(".header__nav");let t,n=window.pageYOffset;window.addEventListener("scroll",(()=>{t=window.pageYOffset;const d=()=>e.classList.contains("header__nav-fixed");t>100&&t>n&&!d()&&e.classList.add("header__nav-fixed"),t<n&&d()&&e.classList.remove("header__nav-fixed"),n=t}))})()}));const e=document.querySelector("#burger"),t=document.querySelector(".navpanel__menu");e.addEventListener("change",(function(){this.checked?t.style.height="400px":t.style.height="0"}));document.querySelectorAll(".navpanel-link").forEach((n=>{n.addEventListener("click",(()=>{e.checked=!1,t.style.height="0"}))}));
-//# sourceMappingURL=index.c0b81d46.js.map
+//# sourceMappingURL=index.72d424f6.js.map
