69f3037021bfa143f1e853e28d686b05e1441592
Siwaaa <siwaa@mail.ru>
2022-04-17 16:05:49 +0300

    fix: title header

---
+++ dist/policy.43ff36c8.js
@@ -0,0 +1,39 @@
+// fixed header
+document.addEventListener('DOMContentLoaded', ()=>{
+    const onScrollHeader = ()=>{
+        const header = document.querySelector('.header__nav');
+        let prevScroll = window.pageYOffset // на сколько была прокручена страница ранее
+        ;
+        let currentScroll // на сколько прокручена страница сейчас
+        ;
+        window.addEventListener('scroll', ()=>{
+            currentScroll = window.pageYOffset;
+            const headerHidden = ()=>header.classList.contains('header__nav-fixed') // узнаем скрыт header или нет
+            ;
+            if (currentScroll > 100 && currentScroll > prevScroll && !headerHidden()) header.classList.add('header__nav-fixed') // то скрываем header
+            ;
+            if (currentScroll < prevScroll && headerHidden()) header.classList.remove('header__nav-fixed') // то отображаем header
+            ;
+            prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
+            ;
+        });
+    };
+    onScrollHeader();
+});
+// BURGER OM-NOM-NOM
+const burgerBtn = document.querySelector('#burger');
+const navbarMenu = document.querySelector('.navpanel__menu');
+burgerBtn.addEventListener('change', function() {
+    if (this.checked) navbarMenu.style.height = '400px';
+    else navbarMenu.style.height = '0';
+});
+// скрываем menu при клике на ссылку-навигацию-якорь
+const linksNavMobile = document.querySelectorAll('.navpanel-link');
+linksNavMobile.forEach((el)=>{
+    el.addEventListener('click', ()=>{
+        burgerBtn.checked = false;
+        navbarMenu.style.height = '0';
+    });
+});
+
+//# sourceMappingURL=policy.43ff36c8.js.map
